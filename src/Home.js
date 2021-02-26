cc1d6aff883798f7af6c2df5d20e2e41ce8083b8
Arturo Reyes <turojkings@gmail.com>
2021-02-26 16:30:23 -0500

    3rd Commit - 2nd row of cards

--- src/Home.js
+++ src/Home.js
@@ -28,9 +28,24 @@ function Home() {
             </div>
 
             <div className='home__section'>
-                <Card />
-                <Card />
-                <Card />
+                <Card 
+                    src="https://media.nomadicmatt.com/2019/airbnb_breakup3.jpg?im_w=720"
+                    title="3 Bedroom Flat in Bournemouth"
+                    description="Superhost with a stunning view of the beachside in Sunny Bournemouth"
+                    price="$130/night"    
+                />
+                <Card 
+                    src="https://thespaces.com/wp-content/uploads/2017/08/Courtesy-of-Airbnb.jpg?im_w=720"
+                    title="Penthouse in London"
+                    description="Enjoy the amazing sights of London with this stunning penthouse"
+                    price="$350/night"
+                />
+                <Card
+                    src="https://media.nomadicmatt.com/2018/apartment.jpg"
+                    title="1 Bedroom apartment"
+                    description="Superhost with great amenities and a fabulous shopping complex nearby?im_w=720"
+                    price="$70/night"  
+                />
             </div>
 
         </div>
