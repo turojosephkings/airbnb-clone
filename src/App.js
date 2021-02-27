34729175c819c17da0ac23294e6bc49fa2a0d615
Arturo Reyes <turojkings@gmail.com>
2021-02-26 20:12:28 -0500

    5th Commit - Search Page Set up

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
+import logo from './logo.svg';
 import './App.css';
-import Home from './Home';
-import Header from './Header';
-import Footer from './Footer';
-
 
 function App() {
-	return (
-
-		// BEM
-		<div className="App">
-		<Header />
-		<Home />
-		<Footer />
-
-
-			{/* Home */}
-				{/* Header */}
-				{/* Banner */}
-					{/* Search */}
-				{/* Cards */}
-				{/* Footer */}
-
-			{/**/}
-		</div>
-	);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/SearchPage.js
@@ -0,0 +1,12 @@
+ import React from 'react';
+ import './SearchPage.css';
+
+function SearchPage() {
+    return (
+        <div className ='searchPage'>
+            <h1>Search page</h1>
+        </div>
+    )
+}
+
+export default SearchPage
