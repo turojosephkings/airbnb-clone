98046ba685e417672ede2cea979d27088d79c15f
Arturo Reyes <turojkings@gmail.com>
2021-02-28 20:36:09 -0500

    8th Commit - Finished Front End Build

--- src/Banner.js
+++ src/Banner.js
@@ -2,9 +2,11 @@ import React, { useState } from 'react';
 import './Banner.css';
 import { Button } from "@material-ui/core";
 import Search from './Search';
+import { useHistory } from 'react-router-dom';
 
 
 function Banner() {
+    const history = useHistory();
     const [showSearch, setShowSearch] = useState(false);
 
     return (
@@ -23,7 +25,7 @@ function Banner() {
                 <h5>
                     Plan a different kind of getaway to uncover the hidden gems near you
                 </h5>
-                <Button variant='outlined'>
+                <Button onClick={() => history.push('/search')} variant='outlined'>
                     Explore Nearby
                 </Button>
             </div>
